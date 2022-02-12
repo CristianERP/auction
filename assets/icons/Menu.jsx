c5d52dc44d308cba95f3f665b3aa9dd69a1c4c20
Cristian Rojas <credrope@hotmail.com>
2022-02-12 18:21:50 -0500

    Agregar icono de menu

---
+++ assets/icons/Menu.jsx
@@ -0,0 +1,8 @@
+const Menu = () => {
+    return ( 
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" style={{ msFilter: "" }}
+        fill="#fff"><path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path></svg>
+     );
+}
+ 
+export default Menu;
