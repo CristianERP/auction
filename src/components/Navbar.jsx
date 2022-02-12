44bfa03bf4cae9fbcb39f05992fedd29516abdba
Cristian Rojas <credrope@hotmail.com>
2022-02-12 18:23:02 -0500

    Ajustar navbar en version movil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,49 @@ import CategoriaIcon from '../../assets/icons/Categoria'
 import Destacado from '../../assets/icons/Destacado'
 import Reciente from '../../assets/icons/Reciente'
 import Money from '../../assets/icons/Dinero'
+import LogIn from '../../assets/icons/LogIn'
+import Menu from '../../assets/icons/Menu'
 import NavButton from './NavButton'
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 const Navbar = () => {
-    return ( 
+    const [isOpen, setIsOpen] = useState(false)
+    return (
         <nav className="flex items-center justify-between p-6 text-xl text-white bg-gray-900 rounded sm:block md:flex md:flex-col lg:flex lg:flex-row">
-            <Link to="/">
+            <Link to="/" className='hidden sm:block'>
                 MY AUCTION
             </Link>
+            <div className="cursor-pointer sm:hidden" onClick={() => setIsOpen(!isOpen)}>
+                <Menu />
+                {
+                    isOpen ?
+                        <ul className="my-4 sm:block md:flex">
+                            <NavButton>
+                                <CategoriaIcon />
+                                <span>Categoria</span>
+                            </NavButton>
+                            <NavButton path="/featured">
+                                <Destacado />
+                                <span>Destacados</span>
+                            </NavButton>
+                            <NavButton>
+                                <Reciente />
+                                <span>Cotizados Recientemente</span>
+                            </NavButton>
+                            <NavButton>
+                                <Money />
+                                <span>Mis subastas</span>
+                            </NavButton>
+                        </ul> :
+                        ""
+                }
+            </div>
             <ul className="hidden my-4 sm:block md:flex">
                 <NavButton>
-                    <CategoriaIcon/>
+                    <CategoriaIcon />
                     <span>Categoria</span>
                 </NavButton>
-                <NavButton>
+                <NavButton path="/featured">
                     <Destacado />
                     <span>Destacados</span>
                 </NavButton>
@@ -29,10 +58,12 @@ const Navbar = () => {
                 </NavButton>
             </ul>
 
-            <Link to="/login" className='p-2 text-black bg-white rounded md:right-0 focus:ring-4'>Iniciar sesión</Link>
-                
+            <Link to="/login" className='p-2 text-black rounded md:right-0 focus:ring-4'>
+                <LogIn />
+            </Link>
+
         </nav>
-     );
+    );
 }
- 
+
 export default Navbar;
