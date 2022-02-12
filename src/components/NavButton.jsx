3d07fe07a0de2e52dad03e7d5fb4d1b12d5c3f47
Cristian Rojas <credrope@hotmail.com>
2022-02-12 18:25:11 -0500

    Usar componente Link en navbar

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,6 +1,8 @@
-const NavButton = ({children}) => {
-    return ( <a href="" className="z-50 flex items-center gap-2 p-2 transition-all rounded justify-items-center hover:ring-1 hover:ring-blue-400">
-    {children}</a> );
+import { Link } from "react-router-dom";
+
+const NavButton = ({path = "", children}) => {
+    return ( <Link to={path} className="z-50 flex items-center gap-2 p-2 transition-all rounded justify-items-center hover:ring-1 hover:ring-blue-400">
+    {children}</Link> );
 }
  
 export default NavButton;
