89d1baa3809d134d3590a8a83785b21bdae1ba09
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:51:11 -0500

    Agregar controlador a componente input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 
 useState
 
-const Input = ({label, type, placeholder, onChange}) => {
+const Input = ({label, type, placeholder, onChange, value}) => {
     
     return ( 
     <div className="mb-4">
         <label className="block mb-2 text-sm font-bold text-grey-darker">{label}:</label>
         <input className="w-full px-3 py-2 border rounded text-grey-darker" type={type}
-        name={label}  onChange={(e) => onChange(e)} placeholder={placeholder} />
+        name={label} value={value} onChange={(e) => onChange(e)} placeholder={placeholder} />
     </div> 
 );
 }
