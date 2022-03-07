1c3e97fed0f376e687d566cf44f8e5d51b070df8
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:58:40 -0500

    Ajuste de componente Home y Myauction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,25 @@
 import Card from '../components/Card';
+import { useState, useEffect } from 'react'
+import getAuctions from '../service/getAuctions';
 
 const Home = () => {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        let isCancelled = false;
+        getAuctions().then(res => {
+            if(!isCancelled){
+                setItems(res)
+            }
+        })
+
+        return () => {
+            isCancelled = true;
+          };
+    },[])
+
+    if(!items) return 'Cargando...'
+    
     return (
         <>
             <section>
@@ -8,7 +27,7 @@ const Home = () => {
             </section>
             <h1 className="my-4 text-2xl font-bold text-white">Destacados</h1>
             <section id="destacados" className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-                <Card />
+                <Card items={items}/>
             </section>
         </>
     );
--- src/pages/Myauction.jsx
+++ src/pages/Myauction.jsx
@@ -1,11 +1,28 @@
-import Sidebar from "../components/Sidebar";
 import Card from "../components/Card";
+import { useState, useEffect } from 'react'
+import getAuctions from "../service/getAuctions";
 
 const Myauction = () => {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        let isCancelled = false;
+        getAuctions().then(res => {
+            if(!isCancelled){
+                setItems(res)
+            }
+        })
+
+        return () => {
+            isCancelled = true;
+          };
+    },[])
+
+    if(!items) return 'Cargando...'
     return (
         <div className="lg:col-span-2 xl:col-span-3">
         <div className="grid gap-2 md:grid lg:grid-cols-2 xl:grid-cols-3">
-            <Card/>
+            <Card items={items}/>
         </div>
         
     </div>  
