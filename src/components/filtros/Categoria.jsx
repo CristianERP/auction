c6280e06cfea58905789f17e407385465266bbf8
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:56:59 -0500

    Ajustar componente Categorias

--- src/components/filtros/Categoria.jsx
+++ src/components/filtros/Categoria.jsx
@@ -1,10 +1,21 @@
+import { useState, useEffect } from 'react'
+import getCategorias from "../../service/getCategorias";
+
 const Categoria = () => {
+    const [categorias, setCategorias] = useState([])
+
+    useEffect(() => {
+       getResult()
+    },[])
+
+    const getResult = async () => {
+        const res = await getCategorias()
+        setCategorias(res)
+    }
+    
     return (
         <ul>
-            <li className="px-3 py-2 my-2 transition rounded-sm cursor-pointer hover:bg-slate-500">Ropa</li>
-            <li className="px-3 py-2 my-2 transition rounded-sm cursor-pointer hover:bg-slate-500">Tecnologia</li>
-            <li className="px-3 py-2 my-2 transition rounded-sm cursor-pointer hover:bg-slate-500">Herramientas</li>
-            <li className="px-3 py-2 my-2 transition rounded-sm cursor-pointer hover:bg-slate-500">Libros</li>
+            {categorias.map((item) => <li className="px-3 py-2 my-2 transition rounded-sm cursor-pointer hover:bg-slate-500" key={item._id} >{item.name}</li>)}
         </ul>
     );
 }
