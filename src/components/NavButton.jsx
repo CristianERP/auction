0f608a6cc0405fad25186c0d3dab4ffa6763376e
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:54:24 -0500

    Agregar acciones a componente NavButton

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
-const NavButton = ({path = "", children}) => {
-    return ( <Link to={path} className="z-50 flex items-center gap-2 p-2 transition-all rounded justify-items-center hover:ring-1 hover:ring-blue-400">
+const NavButton = ({path = "", children, onMouseEnter, onMouseLeave}) => {
+    return ( <Link onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} to={path} className="relative z-50 flex items-center gap-2 p-2 transition-all rounded justify-items-center hover:ring-1 hover:ring-blue-400">
     {children}</Link> );
 }
  
