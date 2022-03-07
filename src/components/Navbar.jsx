6afca4fbfed803ee34615babd494b872bd644f14
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:52:23 -0500

    Refinar componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,29 @@ import Money from '../../assets/icons/Dinero'
 import LogIn from '../../assets/icons/LogIn'
 import Menu from '../../assets/icons/Menu'
 import NavButton from './NavButton'
+import Logo from './Logo'
+import Logout from "../components/Logout";
 import { Link } from 'react-router-dom';
 import { useState } from "react";
-const Navbar = () => {
+
+import CategoriaList from './CategoriaList'
+const Navbar = ({ user }) => {
     const [isOpen, setIsOpen] = useState(false)
+    const [showCategories, setShowCategories] = useState(false)
+
+
+    const handleEnter = () => {
+        setShowCategories(true)
+    }
+
+    const handleLeave = () =>{
+        setShowCategories(false)
+    }
+    
     return (
         <nav className="flex items-center justify-between p-6 text-xl text-white bg-gray-900 rounded sm:block md:flex md:flex-col lg:flex lg:flex-row">
             <Link to="/" className='hidden sm:block'>
-                MY AUCTION
+                <Logo />
             </Link>
             <div className="cursor-pointer sm:hidden" onClick={() => setIsOpen(!isOpen)}>
                 <Menu />
@@ -40,9 +55,10 @@ const Navbar = () => {
                 }
             </div>
             <ul className="hidden my-4 sm:block md:flex">
-                <NavButton>
+                <NavButton onMouseEnter={handleEnter}  onMouseLeave={handleLeave} path='/'>
                     <CategoriaIcon />
-                    <span>Categoria</span>
+                    <span >Categoria</span>
+                    {showCategories ? <CategoriaList onMouseLeave={handleLeave}/>:''}
                 </NavButton>
                 <NavButton path="/featured">
                     <Destacado />
@@ -57,10 +73,11 @@ const Navbar = () => {
                     <span>Mis subastas</span>
                 </NavButton>
             </ul>
+            {user ? <Logout email={user.userEmail}/> :
+                <Link to="/login" className='p-2 text-black rounded md:right-0 focus:ring-4'>
+                    <LogIn />
+                </Link>}
 
-            <Link to="/login" className='p-2 text-black rounded md:right-0 focus:ring-4'>
-                <LogIn />
-            </Link>
 
         </nav>
     );
