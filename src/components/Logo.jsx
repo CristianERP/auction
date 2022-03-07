b090d1aed3582345fd12990313992803577224d0
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:51:32 -0500

    Agregar componente Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import loguito from '../../assets/images/Logo.jpeg'
+
+const Logo = () => {
+    return (
+        <img src={loguito} alt="" className='mx-auto rounded-full h-14 w-14'/>
+     );
+}
+ 
+export default Logo;
