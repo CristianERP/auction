9b431acad58989e29c818b812ca424b54734d31e
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-03-06 22:55:11 -0500

    Agregar controlador de input de busqueda

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,18 +3,19 @@ import Filtro from "./Filtro"
 import Precio from "./filtros/Precio"
 import Categoria from "./filtros/Categoria"
 import Locacion from "./filtros/Locacion"
+import axios from "axios"
 
 
 
-const Sidebar = () => {
+const Sidebar = ({handleSearch, keyword}) => {
     const [openCategoria, setOpencategoria] = useState(false)
     const [openPrecio, setOpenPrecio] = useState(false)
     const [openLocacion, setOpenLocacion] = useState(false)
-    
+
     return (
         <>
             <form>
-                <input type="text" className="w-full py-1 pl-4 bg-white border rounded-sm shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar palabra clave" />
+                <input type="text" className="w-full py-1 pl-4 bg-white border rounded-sm shadow-sm placeholder:italic placeholder:text-slate-400 border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar palabra clave" value={keyword} onChange={handleSearch} />
             </form>
             <div className="mt-4 text-white">
                 <ul>
